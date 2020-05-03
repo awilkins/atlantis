8eca2669d7d126290bbc032dfc4966d3cfddd190
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2020-05-03 18:18:01 +0100

    More pieces of the client implemented

--- server/events/vcs/codecommit_client.go
+++ server/events/vcs/codecommit_client.go
@@ -2,35 +2,127 @@ package vcs
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/service/codecommit"
+	"github.com/aws/aws-sdk-go/service/codecommit/codecommitiface"
 	"github.com/pkg/errors"
 
 	"github.com/runatlantis/atlantis/server/events/models"
+	"github.com/runatlantis/atlantis/server/events/vcs/common"
 )
 
 type CodeCommitClient struct {
-	client *codecommit.CodeCommit
+	Client  codecommitiface.CodeCommitAPI
+	UserArn string
 }
 
 func (c *CodeCommitClient) GetModifiedFiles(repo models.Repo, pull models.PullRequest) ([]string, error) {
 	return []string{}, errors.New("Not Implemented")
 }
 
+const commentSizeLimit int = 1000
+const sepEnd string = "\n```\n</details>" +
+	"\n<br>\n\n**Warning**: Output length greater than max comment size. Continued in next comment."
+const sepStart string = "Continued from previous comment.\n<details><summary>Show Output</summary>\n\n" +
+	"```diff\n"
+
 func (c *CodeCommitClient) CreateComment(repo models.Repo, pullNum int, comment string) error {
-	return errors.New("Not Implemented")
 	// API : PostCommentForPullRequest
+
+	repoName := repo.Name
+	pullRequestId := strconv.Itoa(pullNum)
+
+	pullRequest, err := c.Client.GetPullRequest(&codecommit.GetPullRequestInput{
+		PullRequestId: &pullRequestId,
+	})
+	if err != nil {
+		return err
+	}
+
+	baseCommitId := *pullRequest.PullRequest.PullRequestTargets[0].MergeBase
+	prBranchTipId := *pullRequest.PullRequest.PullRequestTargets[0].SourceCommit
+
+	comments := common.SplitComment(comment, commentSizeLimit, sepEnd, sepStart)
+	for _, commentFragment := range comments {
+		_, err := c.Client.PostCommentForPullRequest(&codecommit.PostCommentForPullRequestInput{
+			RepositoryName: &repoName,
+			PullRequestId:  &pullRequestId,
+			BeforeCommitId: &baseCommitId,
+			AfterCommitId:  &prBranchTipId,
+			Content:        &commentFragment,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (c *CodeCommitClient) HidePrevPlanComments(repo models.Repo, pullNum int) error {
-	return errors.New("Not Implemented")
 	// API : Not sure CC has an equivalent to minimizeComment()
 	// but you can do DeleteCommentContent and UpdateComment
+	var allComments []*codecommit.Comment
+	pullRequestId := strconv.Itoa(pullNum)
+	var nextToken *string = nil
+	for {
+		response, err := c.Client.GetCommentsForPullRequest(&codecommit.GetCommentsForPullRequestInput{
+			PullRequestId: &pullRequestId,
+			NextToken:     nextToken,
+		})
+		if err != nil {
+			return err
+		}
+		allComments = append(allComments, response.CommentsForPullRequestData[0].Comments...)
+		if response.NextToken == nil {
+			break
+		}
+		nextToken = response.NextToken
+	}
+
+	for _, comment := range allComments {
+		if comment.AuthorArn != nil && !strings.EqualFold(*comment.AuthorArn, c.UserArn) {
+			continue
+		}
+		body := strings.Split(*comment.Content, "\n")
+		if len(body) == 0 {
+			continue
+		}
+		firstLine := strings.ToLower(body[0])
+		if !strings.Contains(firstLine, models.PlanCommand.String()) {
+			continue
+		}
+		_, err := c.Client.DeleteCommentContent(&codecommit.DeleteCommentContentInput{
+			CommentId: comment.CommentId,
+		})
+		if err != nil {
+			return errors.Wrapf(err, "minimize conmment %s", *comment.CommentId)
+		}
+	}
+
+	return nil
 }
 
 func (c *CodeCommitClient) PullIsApproved(repo models.Repo, pull models.PullRequest) (bool, error) {
-	return false, errors.New("Not Implemented")
-	// API : EvaluatePullRequestApprovalRules
+
+	pullRequestId := strconv.Itoa(pull.Num)
+
+	pr, err := c.Client.GetPullRequest(&codecommit.GetPullRequestInput{
+		PullRequestId: &pullRequestId,
+	})
+	if err != nil {
+		return false, err
+	}
+	revisionId := *pr.PullRequest.RevisionId
+	eval, err := c.Client.EvaluatePullRequestApprovalRules(&codecommit.EvaluatePullRequestApprovalRulesInput{
+		PullRequestId: &pullRequestId,
+		RevisionId:    &revisionId,
+	})
+	if err != nil {
+		return false, err
+	}
+	return *eval.Evaluation.Approved, nil
 }
 
 func (c *CodeCommitClient) PullIsMergeable(repo models.Repo, pull models.PullRequest) (bool, error) {
@@ -56,6 +148,10 @@ func (c *CodeCommitClient) MarkdownPullLink(pull models.PullRequest) (string, er
 	// For repo in the same acct/region the path works
 	// e.g. /codesuite/codecommit/repositories/atlantis-test/pull-requests/2
 	return fmt.Sprintf("/codesuite/codecommit/repositories/%s/pull-requests/%d", pull.BaseRepo.Name, pull.Num), nil
+}
+
+type mockCodeCommit struct {
+	codecommitiface.CodeCommitAPI
 }
 
 func NewCodeCommitClient() (*CodeCommitClient, error) {
--- server/events/vcs/codecommit_client_test.go
+++ server/events/vcs/codecommit_client_test.go
@@ -1,12 +1,175 @@
 package vcs_test
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/service/codecommit"
+	"github.com/aws/aws-sdk-go/service/codecommit/codecommitiface"
 	"github.com/runatlantis/atlantis/server/events/models"
 	"github.com/runatlantis/atlantis/server/events/vcs"
 	. "github.com/runatlantis/atlantis/testing"
 )
+
+type postCommentMock struct {
+	codecommitiface.CodeCommitAPI
+	PageCount int
+}
+
+func (m *postCommentMock) GetPullRequest(input *codecommit.GetPullRequestInput) (*codecommit.GetPullRequestOutput, error) {
+	baseRevision := "base-revision"
+	prTipRevision := "pr-tip-revision"
+	return &codecommit.GetPullRequestOutput{
+		PullRequest: &codecommit.PullRequest{
+			PullRequestTargets: []*codecommit.PullRequestTarget{
+				{
+					MergeBase:    &baseRevision,
+					SourceCommit: &prTipRevision,
+				},
+			},
+		},
+	}, nil
+}
+
+func (m *postCommentMock) PostCommentForPullRequest(input *codecommit.PostCommentForPullRequestInput) (*codecommit.PostCommentForPullRequestOutput, error) {
+	m.PageCount++
+	return nil, nil
+}
+
+func TestCodeCommitClient_PaginatesComments(t *testing.T) {
+	mock := &postCommentMock{}
+	client := &vcs.CodeCommitClient{
+		Client: mock,
+	}
+	repo := models.Repo{
+		Name: "atlantis-test",
+	}
+	pullNum := 1
+	// limit is 1,000, so 20 chars * 100 should split across *3* pages (because of header/footer)
+	comment := strings.Repeat("99 bottles of beer!\n", 100)
+	err := client.CreateComment(repo, pullNum, comment)
+	Ok(t, err)
+	Equals(t, 3, mock.PageCount)
+}
+
+type hideCommentsMock struct {
+	codecommitiface.CodeCommitAPI
+	pagesRemaining  int
+	commentsDeleted int
+}
+
+func (m *hideCommentsMock) GetCommentsForPullRequest(input *codecommit.GetCommentsForPullRequestInput) (*codecommit.GetCommentsForPullRequestOutput, error) {
+	atlantisArn := "arn:aws:iam::account-id:user/atlantis"
+	userArn := "arn:aws:iam::account-id:user/fred.bloggs"
+	commentId := "a-comment-from-atlantis"
+	planContent := "a cunning plan\nis never revealed\ntoo soon\n"
+	output := codecommit.GetCommentsForPullRequestOutput{
+		CommentsForPullRequestData: []*codecommit.CommentsForPullRequest{
+			{
+				PullRequestId: input.PullRequestId,
+				Comments: []*codecommit.Comment{
+					{
+						AuthorArn: &userArn,
+					},
+					{
+						AuthorArn: &atlantisArn,
+						CommentId: &commentId,
+						Content:   &planContent,
+					},
+				},
+			},
+		},
+	}
+	if m.pagesRemaining > 0 {
+		m.pagesRemaining--
+		nextToken := fmt.Sprintf("next-token-%d", m.pagesRemaining)
+		output.NextToken = &nextToken
+	}
+
+	return &output, nil
+}
+
+func (m *hideCommentsMock) DeleteCommentContent(*codecommit.DeleteCommentContentInput) (*codecommit.DeleteCommentContentOutput, error) {
+	m.commentsDeleted++
+	return nil, nil
+}
+
+func TestCodeCommitClient_HideOldComments(t *testing.T) {
+	mock := hideCommentsMock{
+		pagesRemaining:  2,
+		commentsDeleted: 0,
+	}
+	client := &vcs.CodeCommitClient{
+		Client:  &mock,
+		UserArn: "arn:aws:iam::account-id:user/atlantis",
+	}
+	repo := models.Repo{}
+	pullNum := 1
+	err := client.HidePrevPlanComments(repo, pullNum)
+	Ok(t, err)
+	Equals(t, 3, mock.commentsDeleted)
+}
+
+type pullIsApprovedMock struct {
+	codecommitiface.CodeCommitAPI
+	RepoName             string
+	PullRequestId        int
+	Approved             bool
+	calledGetPullRequest bool
+	calledEvaluate       bool
+}
+
+func (m *pullIsApprovedMock) GetPullRequest(input *codecommit.GetPullRequestInput) (*codecommit.GetPullRequestOutput, error) {
+	m.calledGetPullRequest = true
+	pullRequestId := strconv.Itoa(m.PullRequestId)
+	revisionId := "mac-and-cheese"
+	return &codecommit.GetPullRequestOutput{
+		PullRequest: &codecommit.PullRequest{
+			PullRequestId: &pullRequestId,
+			RevisionId:    &revisionId,
+		},
+	}, nil
+}
+
+func (m *pullIsApprovedMock) EvaluatePullRequestApprovalRules(input *codecommit.EvaluatePullRequestApprovalRulesInput) (*codecommit.EvaluatePullRequestApprovalRulesOutput, error) {
+	m.calledEvaluate = true
+	return &codecommit.EvaluatePullRequestApprovalRulesOutput{
+		Evaluation: &codecommit.Evaluation{
+			Approved: &m.Approved,
+		},
+	}, nil
+}
+
+func (m *pullIsApprovedMock) Used() bool {
+	return m.calledEvaluate && m.calledGetPullRequest
+}
+
+func TestCodeCommitClient_PullIsApproved(t *testing.T) {
+	// Approved
+	mock := &pullIsApprovedMock{
+		Approved: true,
+	}
+	client := vcs.CodeCommitClient{
+		Client: mock,
+	}
+	approved, err := client.PullIsApproved(models.Repo{}, models.PullRequest{})
+	Ok(t, err)
+	Equals(t, true, approved)
+	Equals(t, true, mock.Used())
+	// Not Approved
+	mock = &pullIsApprovedMock{
+		Approved: false,
+	}
+	client = vcs.CodeCommitClient{
+		Client: mock,
+	}
+	approved, err = client.PullIsApproved(models.Repo{}, models.PullRequest{})
+	Ok(t, err)
+	Equals(t, false, approved)
+	Equals(t, true, mock.Used())
+}
 
 func TestCodeCommitClient_MarkdownPullLink(t *testing.T) {
 	client, err := vcs.NewCodeCommitClient()
